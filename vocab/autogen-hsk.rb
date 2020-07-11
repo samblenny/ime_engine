afbedd52a50c9ca1f03d77464e92888d25e1b7b2
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2020-07-11 18:50:15 -0500

    autogen phase 1: normalize pinyin with qc file

---
+++ vocab/autogen-hsk.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/ruby
+# coding: utf-8
+require 'set'
+
+RUST_FILE = "../src/autogen-hsk.rs"
+HSK1 = {src: "hsk1.tsv", qc: "hsk1-QC-do-not-edit.tsv"}
+
+# Returns array: [[hanzi, pinyin], [hanzi, pinyin], ...]
+def read_tsv(file)
+  File.read(file).lines.map { |n| n.chomp.split("\t") }
+end
+
+# Make a string of each unique character used in pinyin from <file>.
+# Result should match TR_FROM (see below)
+def detect_chars(file)
+  Set.new(read_tsv(file).map { |_, pinyin| pinyin.downcase.chars }
+            .flatten).to_a.sort.join("")
+end
+
+# Normalize pinyin to lowercase ASCII (remove diacritics/whitespace/punctuation).
+TR_FROM = " 'abcdefghijklmnopqrstuwxyzàáèéìíòóùúāēěīōūǎǐǒǔǚ"
+TR_TO   = " 'abcdefghijklmnopqrstuwxyzaaeeiioouuaeeiouaiouv"
+ELIDE = " '"
+def normalize(pinyin)
+  n = pinyin.downcase.delete(ELIDE).tr(TR_FROM, TR_TO)
+  abort "Error: normalize(#{pinyin}) gave #{n} (non-ascii). Check TR_FROM & TR_TO." if !n.ascii_only?
+  return n
+end
+
+# Check integrity and coverage of the character transposition table
+detected = detect_chars(HSK1[:src])
+if detected != TR_FROM
+  warn "Error: Characters used in pinyin of #{HSK1[:src]} do not match TR_FROM"
+  warn " detected: \"#{detected}\""
+  warn " TR_FROM:  \"#{TR_FROM}\""
+  abort "You need to update TR_FROM and TR_TO so pinyin will properly normalized to ASCII"
+end
+abort "Error: Check for TR_FROM/TR_TO length mismatch" if TR_FROM.size != TR_TO.size
+
+
+# Make a quality check TSV file for manually checking the normalized pinyin
+print "This will overwrite #{HSK1[:qc]}\nProceed? [y/N]: "
+abort "no changes made" if !["y", "Y"].include? gets.chomp
+
+File.open(HSK1[:qc], "w") { |qc|
+  for hanzi, pinyin in read_tsv(HSK1[:src])
+    qc.puts "#{hanzi}\t#{pinyin}\t#{normalize(pinyin)}"
+  end
+}
