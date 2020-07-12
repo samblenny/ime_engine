6e453824a78d4b2c7a41e1c6c78c0b407ab4cd4e
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2020-07-12 02:10:01 -0500

    reduce repaints

--- www/main.js
+++ www/main.js
@@ -7,9 +7,11 @@ WebAssembly.instantiateStreaming(fetch('ime-engine.wasm'))
         let inbox = exports.js_inbox_ptr();
         let utf8dec = new TextDecoder();
         let x = document.querySelector("#x");
+        let lines = [];
         for (let n=0; n<151; n++) {
             let size = exports.keystroke_event(n);
             let message = utf8dec.decode(buffer.subarray(inbox, inbox + size));
-            x.textContent = [x.textContent, message].join("\n");
+            lines.push(message);
         }
+        x.textContent = lines.join("\n");
     });
