dd50c876da065ce9ec799b59d33dff0cd9ab3b3d
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2020-07-14 02:39:45 -0500

    improve main.js readability

--- wasm-demo/main.js
+++ wasm-demo/main.js
@@ -1,33 +1,6 @@
 "use strict";
 
-function wasmDemo(result) {
-    let exports = result.instance.exports;
-    let wasmShared = new Uint8Array(exports.memory.buffer);
-    let wasmInbox = exports.wasm_inbox_ptr();
-    let wasmOutbox = exports.wasm_outbox_ptr();
-    let mailboxMaxSize = exports.wasm_mailbox_size();
-    let utf8dec = new TextDecoder();
-    let utf8enc = new TextEncoder();
-    let x = document.querySelector("#x");
-    function send(str) {
-        let utf8Message = utf8enc.encode(str);
-        let inboxMsgSize = 0;
-        for (let i=0; i<utf8Message.length && i<mailboxMaxSize; i++) {
-            wasmShared[wasmInbox+i] = utf8Message[i];
-            inboxMsgSize += 1;
-        }
-        let outboxMsgSize = exports.exchange_messages(inboxMsgSize);
-        return utf8dec.decode(wasmShared.subarray(wasmOutbox, wasmOutbox + outboxMsgSize));
-    }
-    let lines = [];
-    let txMessages = ["he", "hao", "na", "shi", "dianshi", "jintian", "shui", "mianbao"];
-    for (const txMsg of txMessages) {
-        let rxMsg = send(txMsg);
-        lines.push(txMsg + "\t" + rxMsg);
-    }
-    x.textContent = lines.join("\n");
-}
-
+// Load WASM module with feature detection, then call wasmDemo()
 if ("instantiateStreaming" in WebAssembly) {
     // The new, more efficient way
     WebAssembly.instantiateStreaming(fetch('ime-engine.wasm'))
@@ -38,4 +11,42 @@ if ("instantiateStreaming" in WebAssembly) {
         .then(response => response.arrayBuffer())
         .then(bytes => WebAssembly.instantiate(bytes))
         .then(wasmDemo);
+}
+
+// Look up the hanzi for a list of pinyin search keys
+function wasmDemo(result) {
+    // Text output goes to pre#stdout
+    let preStdout = document.querySelector("#stdout");
+
+    // Shared memory bindings for JS <--> WASM message passing using UTF-8 strings
+    let exports = result.instance.exports;
+    let wasmShared = new Uint8Array(exports.memory.buffer);
+    let wasmInbox = exports.wasm_inbox_ptr();
+    let wasmOutbox = exports.wasm_outbox_ptr();
+    let mailboxMaxSize = exports.wasm_mailbox_size();
+
+    // send is a synchronous message passing function
+    //   str: string to be sent from JS --> WASM
+    //   returns: reply string from WASM --> JS
+    let utf8dec = new TextDecoder();
+    let utf8enc = new TextEncoder();
+    function send(str) {
+        let utf8Message = utf8enc.encode(str);
+        let inboxMsgSize = 0;
+        for (let i=0; i<utf8Message.length && i<mailboxMaxSize; i++) {
+            wasmShared[wasmInbox+i] = utf8Message[i];
+            inboxMsgSize += 1;
+        }
+        let outboxMsgSize = exports.exchange_messages(inboxMsgSize);
+        return utf8dec.decode(wasmShared.subarray(wasmOutbox, wasmOutbox + outboxMsgSize));
+    }
+
+    // Send search keys to the WASM module and log the response text in pre#stdout
+    let lines = [];
+    let pinyinSearchKeys = ["he", "hao", "na", "shi", "dianshi", "jintian", "shui", "mianbao"];
+    for (const messageToWASM of pinyinSearchKeys) {
+        let replyToJS = send(messageToWASM);
+        lines.push("[js->wasm]: \"" + messageToWASM + "\"\t[wasm->js]: \"" + replyToJS + "\"");
+    }
+    preStdout.textContent = lines.join("\n");
 }
