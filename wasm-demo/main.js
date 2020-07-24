41f0dafc777194692de3fcb6122c8be7de175bb2
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2020-07-23 22:43:44 -0500

    wasm demo: first pass at aria & better interaction; #22, #18

--- wasm-demo/main.js
+++ wasm-demo/main.js
@@ -1,3 +1,4 @@
+"use strict";
 import { loadIMEEngineWasm, syncMessages } from './ime-engine.js';
 
 // HTML UI elements for a chat window
@@ -9,15 +10,49 @@ const compose = document.querySelector("#compose");
 const historySize = 100;
 const historyBuffer = [];
 
-// Update the chat log
-function logMessages(jsToWasm, wasmToJs) {
-    historyBuffer.push("<pinyin> " + jsToWasm);
-    historyBuffer.push(" <hanzi> " + wasmToJs);
-    // Discard oldest messages if buffer is full
-    while (historyBuffer.length > historySize) { historyBuffer.shift(); }
-    // Update UI, scroll to bottom to show new messages
-    chatLog.textContent = historyBuffer.join("\n");
+// Append plain text to chat log and scroll so it is visible.
+// This is uses `Node.textContent = ...` to escape user input (anti-XSS precaution).
+function chatLogPlainText(unsafeMessage) {
+    let p = document.createElement('p');
+    p.textContent = unsafeMessage;
+    chatLog.insertAdjacentElement('beforeend', p);
     chatLog.scrollTop = chatLog.scrollHeight;
+}
+
+// Append HTML to chat log and scroll so it is visible.
+// CAUTION: No XSS protection! Unsafe for user input.
+//          Only use this with safe HTML from internal functions.
+function chatLogSafeHTML(html) {
+    chatLog.insertAdjacentHTML('beforeend', `<div class="cmd">${html}</div>`);
+    chatLog.scrollTop = chatLog.scrollHeight;
+}
+
+function showHelp() {
+    chatLogSafeHTML('<h2>Help</h2>' +
+                    '<p>Pinyin:</p>' +
+                    '<ol><li>Use lowercase letters a to z</li>' +
+                    '<li>Tone marks: type <strong>a</strong> for ā, á, ǎ, or à</li>' +
+                    '<li>Umlaut: type <strong>v</strong> for ü or ǚ</li>' +
+                    '<li>Pick from suggestions: number keys, tab, or space</li>' +
+                    '<li>Send: return key (RET) or enter.</li></ol>' +
+                    '<p>Example: woxiang he guozhi RET ➡ 我想喝果汁</p>' +
+                    '<p>Commands: /help /about</p>');
+}
+
+function showAbout() {
+    const GHDHref = 'https://github.com/samblenny/ime-engine/tree/master/wasm-demo/';
+    const GHDLinkText = 'samblenny/ime-engine/wasm-demo';
+    const GHRHref = 'https://github.com/samblenny/ime-engine/tree/master/README.md';
+    const GHRLinkText = 'samblenny/ime-engine/README.md';
+    const WKHref = 'https://en.wikipedia.org/wiki/Input_method';
+    const WKLinkText = 'Input Method Editor';
+    // Look closely: This uses both template string (`...`) and regular ('...')
+    chatLogSafeHTML('<h2>About</h2>' +
+                    '<p>How does this work?<br>' +
+                    `&nbsp; demo source code on GitHub: <a href="${GHDHref}">${GHDLinkText}</a><br>` +
+                    `&nbsp; project README on GitHub: <a href="${GHRHref}">${GHRLinkText}</a></p>` +
+                    '<p>What is an IME?<br>' +
+                    `&nbsp; Wikipedia's <a href="${WKHref}">${WKLinkText}</a> article</p>`);
 }
 
 // Register event handlers to enable chat mode UI
@@ -26,30 +61,41 @@ function enableChatMode() {
     compose.addEventListener('input', (e) => {
         // TODO: better fix (vs toLowerCase) for when iOS auto-capitalizes input text
         const jsToWasm = compose.value.toLowerCase();
-        suggest.textContent = (jsToWasm != "") ? syncMessages(jsToWasm) : "";
+        if (['/help', '/about'].includes(jsToWasm)) {
+            suggest.textContent = "now type return or enter";
+        } else {
+            const wasmToJs = syncMessages(jsToWasm);
+            suggest.textContent = (wasmToJs && (wasmToJs !== "...")) ? wasmToJs : "";
+        }
     });
     // Update chat log for Enter/Return (send)
     compose.addEventListener('keydown', (e) => {
-        if (!e.repeat && e.key == "Enter" && compose.value != "") {
+        if (!e.repeat && e.key == "Enter" && compose.value.trim() != "") {
             suggest.textContent = "";
             // TODO: better fix (vs toLowerCase) for when iOS auto-capitalizes input text
             const jsToWasm = compose.value.toLowerCase();
             const wasmToJs = syncMessages(jsToWasm);
+            const rawInput = compose.value;
             compose.value = "";
-            logMessages(jsToWasm, wasmToJs);
+            if ('/help' === jsToWasm) {
+                showHelp();
+            } else if ('/about' === jsToWasm) {
+                showAbout();
+            } else if (wasmToJs === "...") {
+                // No match for the pinyin ==> use the input string
+                chatLogPlainText(rawInput.trim());
+            } else {
+                // Pinyin match ==> use result string from wasm
+                chatLogPlainText(wasmToJs);
+            }
         }
     });
 }
 
-// Look up hanzi for a list of pinyin examples, then enable chat mode
+// Show a welcome message, then enable chat mode
 function wasmDemo() {
-    // Send pinyin search keys to the WASM module and log the hanzi responses
-    let lines = [];
-    let pinyinSearchKeys = ["hao", "na", "ta", "nin", "mai", "jintian", "shui", "mianbao"];
-    for (const jsToWasm of pinyinSearchKeys) {
-        const wasmToJs = syncMessages(jsToWasm);
-        logMessages(jsToWasm, wasmToJs);
-    }
+    chatLogSafeHTML("<p>This terminal has a built in Simplified Chinese IME.<br>" +
+                    "You can type pinyin phrases, other text, or /commands.</p>");
     enableChatMode();
 }
 
