6cb817c4b5685bdcf5f69f08a61820e3b493cca3
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2020-07-28 04:36:36 -0500

    first pass at choice picking algo/UI; #16

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,7 @@ mod autogen_hsk;
 type CiyuIndex = usize;
 
 // Shared mailbox buffers for copying messages in & out of the VM
-pub const MAILBOX_SIZE: usize = 200;
+pub const MAILBOX_SIZE: usize = 150;
 pub static mut WASM_INBOX: [u8; MAILBOX_SIZE] = [0; MAILBOX_SIZE];
 pub static mut WASM_OUTBOX: [u8; MAILBOX_SIZE] = [0; MAILBOX_SIZE];
 static mut OUTBOX_BYTES: usize = 0;
@@ -133,6 +133,87 @@ impl<'a> Utf8Str<'a> {
     }
 }
 
+// Data structure for tracking lexemes of query input and their meanings.
+// TokenQueue is no_std, stack-only substitute for Vec<Token>. If TokenQueue
+// were Vec<Token>, it would require heap allocation and linking std.
+pub mod lex {
+    const TOKEN_QUEUE_SIZE: usize = crate::MAILBOX_SIZE;
+    // Holds one Token.
+    #[derive(Copy, Clone)]
+    pub enum Token {
+        CiOne(crate::CiyuIndex),
+        CiOpenChoice(crate::CiyuIndex),
+        MaybeChoice(char),
+        Other(char),
+        Skip,
+    }
+    // Holds queue of Tokens (append only)
+    pub struct TokenQueue {
+        pub queue: [Token; TOKEN_QUEUE_SIZE],
+        pub count: usize,
+    }
+    impl TokenQueue {
+        // Initialize queue.
+        pub fn new() -> TokenQueue {
+            TokenQueue {
+                queue: [Token::Skip; TOKEN_QUEUE_SIZE],
+                count: 0,
+            }
+        }
+        // Add Token to queue.
+        pub fn push(&mut self, tk: Token) -> bool {
+            if self.count < TOKEN_QUEUE_SIZE {
+                self.queue[self.count] = tk;
+                self.count += 1;
+                true
+            } else {
+                // Error: Queue is full
+                false
+            }
+        }
+        // Iterate through tokens, resolve choices, render to oubox.
+        // Side-effect: Send strings to outbox buffer.
+        // Possible surprising behavior:
+        // - Value of CiOpenChoice depends on lookahead for MaybeChoice
+        // - MaybeChoice gets consumed (skipped) if used to resolve choice
+        pub fn render_and_send(&mut self) {
+            let mut current = 0;
+            let mut utf8_buf = [0u8; 4];
+            while current < self.count {
+                match self.queue[current] {
+                    Token::CiOne(ciyu_i) => crate::send(&crate::autogen_hsk::CIYU[ciyu_i]),
+                    Token::CiOpenChoice(ciyu_i) => {
+                        let ciyu = &crate::autogen_hsk::CIYU[ciyu_i];
+                        // Look ahead for choice pick
+                        let mut choice_resolved = false;
+                        for i in current..self.count {
+                            if let Token::MaybeChoice(tk) = self.queue[i] {
+                                match crate::expand_choice_and_send(ciyu, tk) {
+                                    crate::ExpandChoiceResult::WasChoice => {
+                                        self.queue[i] = Token::Skip;
+                                        choice_resolved = true;
+                                        break;
+                                    }
+                                    crate::ExpandChoiceResult::WasNotChoice => {}
+                                }
+                            }
+                        }
+                        if !choice_resolved {
+                            // TODO: use enum variant instead of '0' to indicate no MaybeChoice found
+                            let _ = crate::expand_choice_and_send(ciyu, '0');
+                        }
+                    }
+                    // Space or number (pass through since not consumed by a CiOpenChoice)
+                    Token::MaybeChoice(tk) => crate::send(tk.encode_utf8(&mut utf8_buf)),
+                    Token::Other(tk) => crate::send(tk.encode_utf8(&mut utf8_buf)),
+                    Token::Skip => {}
+                }
+                current += 1;
+            } // end while
+        } // end render_and_send()
+    } // end impl TokenQueue
+} // end lex
+
 // Find longest 词语 match in start..end character window of query buffer.
 // Side-effect: None.
 // Return: (index in 词语 array for match, end boundary character in query for match)
@@ -151,50 +232,106 @@ fn longest_match(query: &Utf8Str, start: usize, mut end: usize) -> Option<(CiyuI
     return None;
 }
 
-// Expand 词语 matches to include choice prompts.
+// Render 词语 multi-matches as resolved choice or prompt for choice.
 // Side-effect: Send results to oubox buffer.
-fn expand_ciyu(ciyu: &str) {
+// Return: Was the maybe_choice token used to resolve a choice?
+enum ExpandChoiceResult {
+    WasChoice,
+    WasNotChoice,
+}
+fn expand_choice_and_send(ciyu: &str, maybe_choice: char) -> ExpandChoiceResult {
     let n = ciyu.split("\t").count();
     if n == 1 {
+        // If this ever happens, there's a bug. Log and recover.
+        trace(901);
         send(ciyu);
-    } else {
-        send(&" (");
+        return ExpandChoiceResult::WasNotChoice;
+    }
+    // Try to pick a choice (falls through if number is not valid)
+    let pick = match maybe_choice {
+        ' ' => 1, // Spacebar picks default option (label=1)
+        '1' => 1,
+        '2' => 2,
+        '3' => 3,
+        '4' => 4,
+        '5' => 5,
+        '6' => 6,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9, // TODO: Fix. This only works for <=9 choices
+        _ => 0,
+    };
+    if pick > 0 {
         for (i, choice) in ciyu.split("\t").enumerate() {
-            send(match i {
-                0 => &"1",
-                1 => &"2",
-                2 => &"3",
-                3 => &"4",
-                4 => &"5",
-                5 => &"6",
-                6 => &"7",
-                7 => &"8",
-                _ => &"9",
-            });
-            send(choice);
-            if i + 1 < n {
-                send(&" ");
+            if i + 1 == pick {
+                send(choice);
+                return ExpandChoiceResult::WasChoice;
             }
         }
-        send(&") ");
     }
+    // Show all choices
+    send(&" (");
+    for (i, choice) in ciyu.split("\t").enumerate() {
+        send(match i {
+            0 => &"1",
+            1 => &"2",
+            2 => &"3",
+            3 => &"4",
+            4 => &"5",
+            5 => &"6",
+            6 => &"7",
+            7 => &"8",
+            _ => &"9",
+        });
+        send(choice);
+        if i + 1 < n {
+            send(&" ");
+        }
+    }
+    send(&") ");
+    return ExpandChoiceResult::WasNotChoice;
 }
 
 // Search for 词语 matches in substrings of query.
 // Side-effect: Send results to outbox buffer.
 #[no_mangle]
-fn search(query: &Utf8Str, mut start: usize, end: usize) {
+fn search(query: &Utf8Str, queue: &mut lex::TokenQueue, mut start: usize, end: usize) {
     while start < end {
         // Limit window size to length of longest phrase in pinyin array
         let window_end = min(start + autogen_hsk::PINYIN_SIZE_MAX, end);
         if let Some((ciyu_i, match_end)) = longest_match(query, start, window_end) {
-            // Match: skip matching portion of query (possibly all of it)
-            expand_ciyu(&autogen_hsk::CIYU[ciyu_i]);
+            // Got Match: push match, continue search in remainder of query
+            if autogen_hsk::CIYU[ciyu_i].contains("\t") {
+                queue.push(lex::Token::CiOpenChoice(ciyu_i));
+            } else {
+                queue.push(lex::Token::CiOne(ciyu_i));
+            }
             start = match_end;
         } else {
-            // No match... skip one character
-            if let Some(skip) = query.char_slice(start, start + 1) {
-                send(skip);
+            // No match... push one character, continue search in remainder of query
+            if let Some(s) = query.char_slice(start, start + 1) {
+                // TODO: Better solution than silently ignoring possible full queue
+                let _ = match s {
+                    " " => queue.push(lex::Token::MaybeChoice(' ')),
+                    "1" => queue.push(lex::Token::MaybeChoice('1')),
+                    "2" => queue.push(lex::Token::MaybeChoice('2')),
+                    "3" => queue.push(lex::Token::MaybeChoice('3')),
+                    "4" => queue.push(lex::Token::MaybeChoice('4')),
+                    "5" => queue.push(lex::Token::MaybeChoice('5')),
+                    "6" => queue.push(lex::Token::MaybeChoice('6')),
+                    "7" => queue.push(lex::Token::MaybeChoice('7')),
+                    "8" => queue.push(lex::Token::MaybeChoice('8')),
+                    "9" => queue.push(lex::Token::MaybeChoice('9')),
+                    _ => {
+                        if let Some(c) = s.chars().nth(0) {
+                            queue.push(lex::Token::Other(c))
+                        } else {
+                            // If this ever happens, there's a bug.
+                            trace(902);
+                            false
+                        }
+                    }
+                };
             }
             start += 1;
         }
@@ -206,9 +343,11 @@ fn search(query: &Utf8Str, mut start: usize, end: usize) {
 #[no_mangle]
 fn look_up(inbox_query: &str) {
     let query = Utf8Str::new(inbox_query);
+    let mut queue = lex::TokenQueue::new();
     let start = 0;
     let end = query.char_count;
-    search(&query, start, end)
+    search(&query, &mut queue, start, end);
+    queue.render_and_send();
 }
 
 // Receive query message, search, put results in outbox.
