4b0eec6637fcb942de52066c262d4db293a36b9a
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2020-08-03 18:24:19 -0500

    Revert "use core::arch::wasm32::unreachable for panic handler; #41"
    This reverts commit 49655e0b5578a7be59a9045cde7a93825d535c99.

--- src/wasm/no_std_bindings.rs
+++ src/wasm/no_std_bindings.rs
@@ -7,20 +7,17 @@
 
 #[link(wasm_import_module = "js")]
 extern "C" {
+    fn js_warn_wasm_panic();
     pub fn js_log_trace(code: i32);
 }
 
 use core::panic::PanicInfo;
 #[panic_handler]
 pub fn panic(_panic_info: &PanicInfo) -> ! {
-    // Rust semantics require that panic handler to never return, and docs for
-    // embedded no_std rust suggest to accomplish that with loop {}. In my
-    // wasm32 testing, loop {} pegs CPU at 100% and makes browser UI
-    // unresponsive. Better alternative is to use WebAssembly unreachable trap
-    // instruction (available in stable since late 2019).
     unsafe {
-        core::arch::wasm32::unreachable();
+        js_warn_wasm_panic();
     }
+    loop {}
 }
 
 // Export location & size of IPC message buffers in VM shared memory
